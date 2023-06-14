49bba2a86db97b76a19854137faff9e80a1a36ed
JoaoSchmitz <jmatheus12349@gmail.com>
2023-06-13 21:57:30 -0300

    criando interface da US-04

---
+++ client/src/pages/EditUserInfo/index.tsx
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import Navbar from "../../components/Navbar";
+import styles from "./styles.module.scss";
+
+export default function EditUserInfo() {
+
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingEmail, setIsEditingEmail] = useState(false);
+  const [isEditingPassword, setIsEditingPassword] = useState(false);
+
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newPasswordConfirmation, setNewPasswordConfirmation] = useState("");
+
+  const [currentPasswordConfirmation, setCurrentPasswordConfirmation] = useState("");
+
+  // Opens the input field for a new name
+  const handleNameEdit = () => {
+    setIsEditingName(!isEditingName);
+    setIsEditingEmail(false);
+    setIsEditingPassword(false);
+
+    setNewName("")
+    setNewEmail("")
+    setNewPassword("")
+    setNewPasswordConfirmation("")
+    setCurrentPasswordConfirmation("")
+  }
+
+  // Opens the input field for a new email
+  const handleEmailEdit = () => {
+    setIsEditingName(false);
+    setIsEditingEmail(!isEditingEmail);
+    setIsEditingPassword(false);
+
+    setNewName("")
+    setNewEmail("")
+    setNewPassword("")
+    setNewPasswordConfirmation("")
+    setCurrentPasswordConfirmation("")
+  }
+
+  // Opens the input field for a new password
+  const handlePasswordEdit = () => {
+    setIsEditingName(false);
+    setIsEditingEmail(false);
+    setIsEditingPassword(!isEditingPassword);
+
+    setNewName("")
+    setNewEmail("")
+    setNewPassword("")
+    setNewPasswordConfirmation("")
+    setCurrentPasswordConfirmation("")
+  }
+
+  // Closes the input field for all user information
+  const handleExitEdition = () => {
+    setIsEditingName(false);
+    setIsEditingEmail(false);
+    setIsEditingPassword(false);
+  }
+
+  return (
+    <Navbar pageName="Minha conta">
+      <div className={styles.pageContainer}>
+        <div className={styles.boxContainer}>
+          <div className={styles.outsideBox}>
+            <div className={styles.insideBox}>
+              <h2>Dados</h2>
+              {
+                isEditingName
+                ?
+                <form className={styles.formsBox}>
+                  <input
+                    type="text"
+                    placeholder="Digite o novo nome..."
+                    className={styles.inputBox}
+                    required
+                    value={newName}
+                    onChange={(event) => setNewName(event.target.value)}
+                  />
+                  <button className={styles.confirmEditButton}>Ok</button>
+                  <button className={styles.exitEditButton} onClick={handleExitEdition}>X</button>
+                </form>
+                :
+                <div className={styles.formsBox}>
+                  <div className={styles.infoBox}>** Nome da pessoa **</div>
+                  <button className={styles.editButton} onClick={handleNameEdit}>Edit</button>
+                </div>
+              }
+              {
+                isEditingEmail
+                ?
+                <form className={styles.formsBox}>
+                  <input
+                    type="text"
+                    placeholder="Digite o novo e-mail..."
+                    className={styles.inputBox}
+                    required
+                    value={newEmail}
+                    onChange={(event) => setNewEmail(event.target.value)}
+                  />
+                  <button className={styles.confirmEditButton}>Ok</button>
+                  <button className={styles.exitEditButton} onClick={handleExitEdition}>X</button>
+                </form>
+                :
+                <div className={styles.formsBox}>
+                  <div className={styles.infoBox}>** Email da pessoa **</div>
+                  <button className={styles.editButton} onClick={handleEmailEdit}>Edit</button>
+                </div>
+              }
+              {
+                isEditingPassword
+                ?
+                <form className={styles.passwordFormsBox}>
+                  <input
+                    type="password"
+                    placeholder="Digite a senha atual..."
+                    className={styles.passwordInputBox}
+                    required
+                    value={currentPasswordConfirmation}
+                    onChange={(event) => setCurrentPasswordConfirmation(event.target.value)}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Digite a nova senha..."
+                    className={styles.passwordInputBox}
+                    required
+                    value={newPassword}
+                    onChange={(event) => setNewPassword(event.target.value)}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Confirme a nova senha..."
+                    className={styles.passwordInputBox}
+                    required
+                    value={newPasswordConfirmation}
+                    onChange={(event) => setNewPasswordConfirmation(event.target.value)}
+                  />
+                  <button className={styles.passwordButton}>Ok</button>
+                  <button className={styles.exitPasswordEditButton} onClick={handleExitEdition}>X</button>
+                </form>
+                :
+                <div className={styles.formsBox}>
+                  <div className={styles.infoBox}>********</div>
+                  <button className={styles.editButton} onClick={handlePasswordEdit}>Edit</button>
+                </div>
+              }
+              <button className={styles.deleteButton}>EXCLUIR CONTA</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Navbar>
+  )
+}
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,6 +3,7 @@ import AuthProviderComponent from "./auth/components/auth-context-component";
 import ProtectedComponent from "./auth/components/protected-component";
 import LoginAndRegister from "./pages/LoginAndRegister";
 import Travels from "./pages/Travels";
+import EditUserInfo from "./pages/EditUserInfo";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -11,6 +12,7 @@ const router = createBrowserRouter(
       <Route element={<AuthProviderComponent />}>
         <Route path="/login-and-register" element={<LoginAndRegister />} />
         {/* Routes that need login need to be inside this route */}
+          <Route path="/user-info" element={<EditUserInfo />} />
         <Route element={<ProtectedComponent />}>
           <Route path="/" element={<Travels />} />
         </Route>
